16d8fb1ad730f205e96a3c114d6c6e57f65f88ac
AlbertPadi <jhoanpadi@gmail.com>
2015-09-23 11:02:35 -0700

    Subiendo Tarea 7

---
+++ EjemploDeJagged/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace EjemploDeJagged
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Guardarbutton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxPromedio = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxMayor = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxMenor = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Salones";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Alumnos";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(23, 189);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 108);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Guardarbutton
+            // 
+            this.Guardarbutton.Location = new System.Drawing.Point(175, 144);
+            this.Guardarbutton.Name = "Guardarbutton";
+            this.Guardarbutton.Size = new System.Drawing.Size(86, 23);
+            this.Guardarbutton.TabIndex = 5;
+            this.Guardarbutton.Text = "Guardar";
+            this.Guardarbutton.UseVisualStyleBackColor = true;
+            this.Guardarbutton.Click += new System.EventHandler(this.Guardarbutton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Calificaciones";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(141, 107);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(120, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Crear Arreglo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxPromedio
+            // 
+            this.textBoxPromedio.Location = new System.Drawing.Point(187, 189);
+            this.textBoxPromedio.Name = "textBoxPromedio";
+            this.textBoxPromedio.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPromedio.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(210, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Promedio";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(217, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Mayor";
+            // 
+            // textBoxMayor
+            // 
+            this.textBoxMayor.Location = new System.Drawing.Point(187, 228);
+            this.textBoxMayor.Name = "textBoxMayor";
+            this.textBoxMayor.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMayor.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(0, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(217, 261);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Menor";
+            // 
+            // textBoxMenor
+            // 
+            this.textBoxMenor.Location = new System.Drawing.Point(187, 277);
+            this.textBoxMenor.Name = "textBoxMenor";
+            this.textBoxMenor.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMenor.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 309);
+            this.Controls.Add(this.textBoxMenor);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxMayor);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxPromedio);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Guardarbutton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Guardarbutton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxPromedio;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxMayor;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxMenor;
+    }
+}
+
---
+++ EjemploDeJagged/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploDeJagged
+{
+    public partial class Form1 : Form
+    {
+        int cont = 0;
+        float prom;
+        float min = 10.0f;
+        float max = 0.0f;
+
+        int salones;
+        int alumnos;
+        int calificacione;
+        int pos;
+        int mostrar;
+        float total;
+        ArrayList calificaciones;
+
+        public Form1()
+        {
+            InitializeComponent();
+            calificaciones = new ArrayList();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            salones = Convert.ToInt32(textBox1.Text);
+            alumnos = Convert.ToInt32(textBox2.Text);
+
+            int[][] salon = new int[salones][];
+
+            for (int i = 0; i < salones; i++)
+            {
+                salon[i] = new int[alumnos];
+            }
+            button1.Text = "Mostrar";
+            
+            if (calificacione > 0)
+            {
+
+                for (int i = 0; i < salon.Length; i++)
+                {
+                    for (int x = 0; x < salon[i].Length; x++)
+                    {
+                        total += salon[i][x] = Int32.Parse(calificaciones[pos++].ToString());
+                        prom = total / cont;
+                        if (max<salon[i][x])
+                        {
+                            max = salon[i][x];
+                        }
+
+                        if (min > salon[i][x])
+                        {
+                            min = salon[i][x];
+                        }
+
+                    }
+                }
+
+                listBox1.Items.Add("Lista de Calificaciones");
+
+                for (int i = 0; i < salon.Length; i++)
+                {
+                    listBox1.Items.Add("Salon " + (i + 1));
+                    for (int x = 0; x < salon[i].Length; x++)
+                    {
+                        listBox1.Items.Add(calificaciones[mostrar++]);
+                    }
+                }
+                listBox1.Items.Add("El Total es: "+total);
+          
+                textBoxPromedio.Text = prom.ToString();
+
+                textBoxMayor.Text = max.ToString();
+
+                textBoxMenor.Text = min.ToString();
+            }
+
+          }
+
+        private void Guardarbutton_Click(object sender, EventArgs e)
+        {
+            
+            cont++;
+            calificacione = Convert.ToInt32(textBox3.Text);
+            calificaciones.Add(calificacione);
+            MessageBox.Show("Guardada.");
+        }
+    }
+}
